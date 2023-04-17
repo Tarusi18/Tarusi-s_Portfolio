e91ffa8fa0e1a86e1b4d0d1d788b629c059aca00
Tarusi18 <90305920+Tarusi18@users.noreply.github.com>
2023-04-17 18:27:35 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,150 @@
+import "./assets/img.jpg";
+
+const logotext = "Tarusi Ambule";
+const meta = {
+  title: "Tarusi Ambule",
+  description:
+    "I’m Tarusi Ambule React Full stack devloper,currently working in ATMECS Technologies Private Limited Inc.",
+};
+
+const introdata = {
+  title: "Hi, I am Tarusi Ambule",
+  animated: {
+    first: "I love coding and It's my passion...",
+    second: "I code cool Websites as well as Mobile Apps",
+    third: "I develop mobile apps",
+  },
+  description:
+    " Hi, I am React Full-Stack Developer Working in ATMECS Technologies Private limited Inc, Hyderabad",
+  your_img_url: `url(${"./assets/img.jpg"})`,
+};
+
+const dataabout = {
+  title: "A bit about My self",
+  aboutme:
+    "I have worked on various projects based on ReactJs for Web App development as well as React Native for Mobile App development. And have experience of 1.5 years in ATMECS Technologies Private Limited Hyderabad. I worked for NFT MarketPlace Project and have a bit knowledge of Blockchain as well as Etherium. I have worked on Mobile App as well as IOS app using React Native, developed Sports App for EPL (England Primier League)",
+};
+const worktimeline = [
+  {
+    jobtitle: "Designer of week",
+    where: "YAdfi",
+    date: "2020",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+  },
+];
+
+const skills = [
+  {
+    name: "ReactJs",
+    value: 90,
+  },
+  {
+    name: "React Native",
+    value: 90,
+  },
+  {
+    name: "Javascript",
+    value: 70,
+  },
+  {
+    name: "Html",
+    value: 90,
+  },
+  {
+    name: "Css",
+    value: 90,
+  },
+  {
+    name: "Jquery",
+    value: 60,
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Design",
+    description:
+      "I have worked on various UI & UX Design as an Full-Stack Developer. I have worked on Ports America Project as well for Development & Implemenation of Mobile App. Also worked on HelathX project for IOS & Mobile App development as per Figma.",
+  },
+  {
+    title: "Mobile App & IOS App",
+    description:
+      "I have Designed and Worked on TFL(The Fan Lounge) App using React Native Technology for Mobile as well as IOS App.Here I have worked on various bug fixes as well as new screen design & implementations. Also Worked on Deployment of this app into PlayStore.",
+  },
+  {
+    title: "Web Design",
+    description:
+      "I have designed Web App for NFT MarketPlace using ReactJs. It has responsive Design as per Client requirement, I have completed the following Project",
+  },
+];
+
+// const dataportfolio = [
+//   {
+//     img: "https://picsum.photos/400/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+//   {
+//     img: "https://picsum.photos/400/800/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+//   {
+//     img: "https://picsum.photos/400/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+//   {
+//     img: "https://picsum.photos/400/600/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+//   {
+//     img: "https://picsum.photos/400/300/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+// ];
+
+const contactConfig = {
+  YOUR_EMAIL: "tarusi.ambule18@gmail.com",
+  YOUR_FONE: "7972922451 / 9623722683",
+  description:
+    "Having any exciting Opportunity !! Feel Free to reach out to me.",
+  YOUR_SERVICE_ID: "service_82km9sn",
+  YOUR_TEMPLATE_ID: "template_vx5ccz5s",
+  YOUR_USER_ID: "Tarusi Ambule",
+  YOUR_PUBLIC_KEY: "0cvs7c_9gR62FtWWx",
+};
+
+const socialprofils = {
+  github: "https://github.com/",
+  facebook: "https://www.facebook.com/tarusi.ambule",
+  linkedin: "https://www.linkedin.com/in/tarusi-ambule-2a6b43199",
+};
+export {
+  meta,
+  dataabout,
+  // dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
