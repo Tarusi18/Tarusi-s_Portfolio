82f7b7c0806ea4c62c62c41eeb2954768ca7530c
Tarusi18 <90305920+Tarusi18@users.noreply.github.com>
2024-09-04 14:34:30 +0530

    Updated

--- src/content_option.js
+++ src/content_option.js
@@ -4,39 +4,40 @@ const logotext = "Tarusi Ambule";
 const meta = {
   title: "Tarusi Ambule",
   description:
-    "I’m Tarusi Ambule a Software Engineer,currently working in ATMECS Global Inc.",
+    "Welcome to the digital portfolio of Tarusi Ambule, a passionate Software Engineer currently contributing to groundbreaking projects at ATMECS Global Inc.",
 };
 
 const introdata = {
-  title: "Hi, I am Tarusi Ambule",
+  title: "Hi, I’m Tarusi Ambule",
   animated: {
-    first: "I love coding and It's my passion...",
-    second: "I code cool Websites as well as Mobile Apps",
-    third: "I develop mobile apps",
+    first: "Crafting immersive web experiences...",
+    second: "Bringing innovative mobile apps to life...",
+    third: "Turning ideas into digital realities...",
   },
   description:
-    " Hi, I am React Full-Stack Developer Working in ATMECS Technologies Private limited Inc, Hyderabad",
+    "As a Full-Stack Developer at ATMECS Technologies Private Limited, Hyderabad, I specialize in React and React Native to build powerful, user-friendly web and mobile applications.",
   your_img_url: `url(${"./assets/img.jpg"})`,
 };
 
 const dataabout = {
-  title: "A bit about My self",
+  title: "A bit about myself",
   aboutme:
-    "I have worked on various projects based on ReactJs for Web App development as well as React Native for Mobile App development. And have experience of 1.5 years in ATMECS Technologies Private Limited Hyderabad. I worked for NFT MarketPlace Project and have a bit knowledge of Blockchain as well as Etherium. I have worked on Mobile App as well as IOS app using React Native, developed Sports App for EPL (England Primier League)",
+    "With over 1.5 years of experience at ATMECS, I have honed my skills in developing dynamic web and mobile apps using ReactJS and React Native. My journey includes working on the NFT Marketplace, where I gained insights into Blockchain and Ethereum, and developing sports apps for the EPL. I thrive in environments where I can blend creativity with technology, consistently delivering high-quality digital solutions.",
 };
+
 const worktimeline = [
   {
-    jobtitle: "Designer of week",
+    jobtitle: "Designer of the Week",
     where: "YAdfi",
     date: "2020",
   },
   {
-    jobtitle: "Designer of week",
+    jobtitle: "Designer of the Week",
     where: "Jamalya",
     date: "2019",
   },
   {
-    jobtitle: "Designer of week",
+    jobtitle: "Designer of the Week",
     where: "ALquds",
     date: "2019",
   },
@@ -44,7 +45,7 @@ const worktimeline = [
 
 const skills = [
   {
-    name: "ReactJs",
+    name: "ReactJS",
     value: 90,
   },
   {
@@ -52,20 +53,20 @@ const skills = [
     value: 90,
   },
   {
-    name: "Javascript",
+    name: "JavaScript",
+    value: 80,
+  },
+  {
+    name: "HTML",
+    value: 90,
+  },
+  {
+    name: "CSS",
+    value: 90,
+  },
+  {
+    name: "jQuery",
     value: 70,
-  },
-  {
-    name: "Html",
-    value: 90,
-  },
-  {
-    name: "Css",
-    value: 90,
-  },
-  {
-    name: "Jquery",
-    value: 60,
   },
 ];
 
@@ -73,59 +74,25 @@ const services = [
   {
     title: "UI & UX Design",
     description:
-      "I have worked on various UI & UX Design as an Full-Stack Developer. I have worked on Ports America Project as well for Development & Implemenation of Mobile App. Also worked on HelathX project for IOS & Mobile App development as per Figma.",
+      "Specializing in crafting intuitive UI & UX designs, I’ve developed and implemented mobile apps for projects like Ports America and HealthX, focusing on both Android and iOS platforms.",
   },
   {
-    title: "Mobile App & IOS App",
+    title: "Mobile & iOS App Development",
     description:
-      "I have Designed and Worked on TFL(The Fan Lounge) App using React Native Technology for Mobile as well as IOS App.Here I have worked on various bug fixes as well as new screen design & implementations. Also Worked on Deployment of this app into PlayStore.",
+      "I’ve led the design and development of The Fan Lounge (TFL) app using React Native, overseeing everything from bug fixes to new screen designs, and deploying the app to the Play Store.",
   },
   {
-    title: "Web Design",
+    title: "Web Development",
     description:
-      "I have designed Web App for NFT MarketPlace using ReactJs. It has responsive Design as per Client requirement, I have completed the following Project",
+      "I designed and developed a responsive web app for the NFT Marketplace using ReactJS, meeting client specifications and delivering a seamless user experience.",
   },
 ];
-
-// const dataportfolio = [
-//   {
-//     img: "https://picsum.photos/400/?grayscale",
-//     description:
-//       "The wisdom of life consists in the elimination of non-essentials.",
-//     link: "#",
-//   },
-//   {
-//     img: "https://picsum.photos/400/800/?grayscale",
-//     description:
-//       "The wisdom of life consists in the elimination of non-essentials.",
-//     link: "#",
-//   },
-//   {
-//     img: "https://picsum.photos/400/?grayscale",
-//     description:
-//       "The wisdom of life consists in the elimination of non-essentials.",
-//     link: "#",
-//   },
-//   {
-//     img: "https://picsum.photos/400/600/?grayscale",
-//     description:
-//       "The wisdom of life consists in the elimination of non-essentials.",
-//     link: "#",
-//   },
-//   {
-//     img: "https://picsum.photos/400/300/?grayscale",
-//     description:
-//       "The wisdom of life consists in the elimination of non-essentials.",
-//     link: "#",
-//   },
-// ];
 
 const contactConfig = {
   YOUR_EMAIL: "tarusi.ambule18@gmail.com",
   YOUR_FONE: "7972922451 / 9623722683",
   description:
-    "Having any exciting Opportunity !! Feel Free to reach out to me.",
-
+    "Excited about potential collaborations or opportunities? Don’t hesitate to reach out!",
 };
 
 const socialprofils = {
@@ -133,10 +100,10 @@ const socialprofils = {
   facebook: "https://www.facebook.com/tarusi.ambule",
   linkedin: "https://www.linkedin.com/in/tarusi-ambule-2a6b43199",
 };
+
 export {
   meta,
   dataabout,
-  // dataportfolio,
   worktimeline,
   skills,
   services,
@@ -145,4 +112,3 @@ export {
   socialprofils,
   logotext,
 };
-
